e80004670f2d2bb549872e107eeda9f978e2cb8d
VanDuy <vanduy@gmail.com>
2023-02-13 22:01:49 +0700

    B01

---
+++ b01/ex01.js
@@ -0,0 +1,73 @@
+let arrImage = [
+    {
+      "id": 1,
+      "name": "Animal Funny 1",
+      "like": 100,
+      "dislike": 5,
+      "love": 50
+    },
+    {
+      "id": 2,
+      "name": "Animal Funny 2",
+      "like": 200,
+      "dislike": 15,
+      "love": 129
+    },
+    {
+      "id": 3,
+      "name": "Animal Funny 3",
+      "like": 100,
+      "dislike": 42,
+      "love": 10
+    },
+    {
+      "id": 4,
+      "name": "Animal Funny 4",
+      "like": 100,
+      "dislike": 22,
+      "love": 44
+    },
+    {
+      "id": 5,
+      "name": "Animal Funny 4",
+      "like": 154,
+      "dislike": 22,
+      "love": 144
+    },
+    {
+      "id": 6,
+      "name": "Animal Funny 6",
+      "like": 88,
+      "dislike": 45,
+      "love": 56
+    },
+    {
+      "id": 7,
+      "name": "Animal Funny 7",
+      "like": 100,
+      "dislike": 43,
+      "love": 44
+    },
+    {
+      "id": 8,
+      "name": "Animal Funny 8",
+      "like": 333,
+      "dislike": 45,
+      "love": 280
+    },
+    {
+      "id": 9,
+      "name": "Animal Funny 9",
+      "like": 145,
+      "dislike": 33,
+      "love": 120
+    },
+    {
+      "id": 10,
+      "name": "Animal Funny 10",
+      "like": 66,
+      "dislike": 44,
+      "love": 44
+    }
+]
+
---
+++ b01/node.md
@@ -0,0 +1,20 @@
+1. Hướng dẫn cách sử dụng Git
+2. Bài tập javascript cơ bản.
+   1. Lấy tất cả các hình ảnh có lượt like khác 100
+   2. Tạo mảng mới theo mảng arrImage và theo thứ tự dislike tăng dần
+   3. In ra tên của các sản phẩm có lượt like và love >= 100
+   4. Thêm xếp loại cho từng hình ảnh :
+      - Loại A : lượt like >= 300
+      - Loại B : 200 <= lượt like <= 300
+      - Loại C : 100 <= lượt like <= 200
+      - Loại D : lượt like <= 100
+   5. Tạo phương thức nhận vào 3 tham số ( like , love , dislike)
+      - Kết quả : Bạn có 0 hình ảnh có lượt like là : 10 , Bạn có 2 hình ảnh có lượt dislike là 20 ,...
+3. Khởi động dự án nodejs
+   - https://expressjs.com/en/starter/generator.html
+   - Nhúng theme AdminLte : https://adminlte.io/
+4. Bài tập về nhà
+   - Xem toàn bộ nodejs level 1
+   - Nhúng theme AdminLte các phần đã xem ở video
+   - Phân tách các phần trong theme
+   - Hoàn thành chức năng CRUD
